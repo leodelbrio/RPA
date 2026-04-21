34e9dd042b76f42ca82a57daf9d361ad471e571f
leodelbrio <leonardodelbicho@gmail.com>
2026-04-21 19:15:36 -0300

    ej2

---
+++ TP1/ej1.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+    System.out.println("HOLAAA");
+}
